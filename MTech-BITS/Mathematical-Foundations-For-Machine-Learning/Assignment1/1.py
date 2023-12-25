0dfc2d1db7343f4fedae7bdfe8618058de5b97bf
Rijumone Choudhuri <mailmeonriju@gmail.com>
2023-12-25 23:06:49 +0700

    Added REF, RREF, pivot, non pivot

--- MTech-BITS/Mathematical-Foundations-For-Machine-Learning/Assignment1/1.py
+++ MTech-BITS/Mathematical-Foundations-For-Machine-Learning/Assignment1/1.py
@@ -7,6 +7,9 @@ Deliverables: The code snippet showing the procedure for REF and RREF. (1 mark +
 """
 
 
+import pdb
+
+
 def construct_augmented_matrix(A, B):
     if len(A) != len(B):
         raise ValueError(
@@ -25,7 +28,7 @@ def construct_augmented_matrix(A, B):
     return augmented_matrix
 
 
-def is_rref(matrix: list) -> bool:
+def is_ref(matrix: list) -> bool:
     """Given a matrix, determine if it is in RREF form.
 
     Args:
@@ -37,22 +40,41 @@ def is_rref(matrix: list) -> bool:
     # First check:
     # If a row does not entirely consists of
     # zeros, than the first non zero number is 1.
-    import pdb;pdb.set_trace()
+    # import pdb;pdb.set_trace()
+    # for row in matrix:
+    #     print(row)
+    
+    # import pdb;pdb.set_trace()
     for row in matrix:
-        print(row)
-    check_one_passed = False
-    for row in matrix:
+        check_one_passed = False
         if len(row) != len([_ for _ in row if int(_) == 0]):
             for element in row:
-                if int(element) == 0:
+                if element in [0, 0.0, -0, -0.0]:
                     continue
-                elif int(element) == 1:
+                elif element in [1, 1.0]:
                     check_one_passed = True
                     break
+                else:
+                    check_one_passed = False
+                    break
+
+        if not check_one_passed:
+            return False
+    
     # Second check:
+    # All zero rows should be on the bottom of the matrix
+    idx_first_zero_row = -1
+    for idx, row in enumerate(matrix):
+        if len(row) == len([_ for _ in row if int(_) == 0]):
+            idx_first_zero_row = idx
+        else:
+            if idx < idx_first_zero_row:
+                return False
+        
     # Third check:
-    if not check_one_passed:
-        return False
+    # In any two successive rows that do not consist entirely of zeroes,
+    # the leading 1 in the lower row occcurs further to the right than the
+    # leading 1 in the higher row.
     last_lead_position = -1
     for row in matrix:
         for j in range(len(row)):
@@ -73,10 +95,11 @@ def matrix_to_ref(matrix: list) -> list:
     Returns:
         list: list of lists of real numbers representing a matrix
     """
-    import pdb;pdb.set_trace()
+    if is_ref(matrix):
+        return matrix
+    
     m = len(matrix)
     n = len(matrix[0])
-
     # Step 1: Locate the leftmost column that does not contain entirely of zeros.
     non_zero_column = 0
     for j in range(n):
@@ -84,7 +107,6 @@ def matrix_to_ref(matrix: list) -> list:
             non_zero_column = j
             break
     
-    # print(non_zero_column)
     non_zero_row = 0
     # Step 2: interchange top row with a row w/ non zero element if needed
     if matrix[0][non_zero_column] == 0:
@@ -95,7 +117,6 @@ def matrix_to_ref(matrix: list) -> list:
                 non_zero_row = i
                 break
     
-    # print(non_zero_row)
     matrix[0], matrix[non_zero_row] = matrix[non_zero_row], matrix[0]
 
     # Step 3: if the entry that is now at the top of the non_zero_column is a != 0,
@@ -114,16 +135,78 @@ def matrix_to_ref(matrix: list) -> list:
         for j in range(n):
             matrix[row][j] += multiplier * matrix[0][j]
     
-    while not is_rref(matrix):
+    
+    while not is_ref(matrix):
         print(f'matrix: {matrix}')
-        matrix = matrix[0] + matrix_to_ref(matrix[1:])
+        matrix = [matrix[0]] + matrix_to_ref(matrix[1:])
     return matrix
 
 
+def ref_to_rref(matrix: list) -> list:
+    m = len(matrix)
+    n = len(matrix[0])
+
+    for row in range(m):
+        leading_col = -1  # Track the column of the leading 1 in this row
+        for column in range(n):
+            if matrix[row][column] in [1, 1.0]:
+                leading_col = column
+                break
+
+        if leading_col != -1:  # If a leading 1 is found
+            # Make all other entries in other rows in the leading column as zero
+            for i in range(m):
+                if i != row and matrix[i][leading_col] not in [0, -0, 0.0, -0.0]:
+                    # find multiplier
+                    multiplier = matrix[i][leading_col]
+                    for j in range(n):
+                        matrix[i][j] -= multiplier * matrix[row][j]
+
+    return matrix
     
+def identify_pivot_non_pivot_cols(matrix: list) -> (list, list):
+    """Given a matrix in its REF or RREF form, identify and return
+    the indices of the pivot columns. Pivot columns are those which have
+    a leading 1 in them, non pivot are the remaining columns.
+
+    Args:
+        matrix (list): list of lists of real numbers representing a matrix
+
+    Returns:
+        list, list: Tuple of lists of lists of real numbers representing a matrix
+    """
+    m = len(matrix)
+    n = len(matrix[0])
+    pivot_cols, non_pivot_cols = [], []
+    for j in range(n):
+        is_pivot = False
+        for i in range(m):
+            if matrix[i][j] == 1 and all(matrix[_][j] in [0, 0.0, -0, -0.0] for _ in range(i)):
+                is_pivot = True
+                break
+        if is_pivot:    pivot_cols.append(j)
+        else:   non_pivot_cols.append(j)
+    return pivot_cols, non_pivot_cols
+
+def main2():
+    pivot_cols, non_pivot_cols = identify_pivot_non_pivot_cols([
+        [1.0, 2.0, 0.0, 3.0, 0.0, 7.0],
+        [0.0, 0.0, 1.0, 0.0, 0.0, 1.0],
+        [0.0, 0.0, 0.0, 0.0, 1.0, 2.0]
+    ])
+    print(pivot_cols, non_pivot_cols)
+
 
 def main1():
-    print(is_rref([[0.0, 0.0, 0.0, 0.0, 1.0, 2.0]]))
+    print(is_ref([
+        [1.0,   2.0,    -5.0,   3.0,    6.0,    14.0], 
+        [0,     0,      -2,     0,      7,      12], 
+        [0.0,   0.0,    5.0,    0.0,    -17.0,  -29.0],
+    ]))
+    print(is_ref([
+        [-0.0, -0.0, 1.0, -0.0, -3.5, -6.0], 
+        [0.0, 0.0, 0.0, 0.0, 0.5, 1.0],
+    ]))
 
 
 def main():
@@ -142,17 +225,28 @@ def main():
     for row in augmented_matrix:
         print(row)
 
-    is_rref_o = is_rref(matrix=augmented_matrix)
-    print(is_rref_o)
+    is_ref_o = is_ref(matrix=augmented_matrix)
+    print(is_ref_o)
 
     ref_matrix = matrix_to_ref(matrix=augmented_matrix)
 
-    print('main')
+
+    is_ref_o = is_ref(matrix=augmented_matrix)
+    print(is_ref_o)
+
+    print('REF')
     for row in ref_matrix:
+        print(row)
+
+    rref_matrix = ref_to_rref(ref_matrix)
+    print('RREF')
+    for row in rref_matrix:
         print(row)
 
 
 
+
 if __name__ == '__main__':
-    main()
+    # main()
     # main1()
+    main2()
