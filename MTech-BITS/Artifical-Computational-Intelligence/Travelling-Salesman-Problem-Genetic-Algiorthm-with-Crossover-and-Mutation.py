74b124a1a2d8d2fe41e9967957c597b498742476
Rijumone Choudhuri <mailmeonriju@gmail.com>
2024-02-11 23:04:36 +0530

    refactored Travelling-Salesman-Problem-Genetic-Algiorthm-with-Crossover-and-Mutation.py

--- MTech-BITS/Artifical-Computational-Intelligence/Travelling-Salesman-Problem-Genetic-Algiorthm-with-Crossover-and-Mutation.py
+++ MTech-BITS/Artifical-Computational-Intelligence/Travelling-Salesman-Problem-Genetic-Algiorthm-with-Crossover-and-Mutation.py
@@ -11,16 +11,6 @@ This code implements a genetic algorithm to solve the travelling salesman proble
 *This process is repeated for a number of generations until a stopping criterion is reached 
  (in this case, a maximum number of generations is defined).
 *The fittest individual in the final population is returned as the solution to the travelling salesman problem.
-The code defines several functions to implement the genetic algorithm, including:
-*fitness(): a function to calculate the fitness of a given genome (i.e. the total distance travelled on the path).
-*select_population(): a function to select a random sample of individuals from a given population.
-*tournament_selection(): a function to select the fittest individuals from a given population using tournament selection.
-*ordered_crossover(): a function to apply ordered crossover to two parent genomes and generate new offspring.
-*mutation(): a function to apply mutation to a given genome.
-*total_population_score(): a function to calculate the total score of a given population 
- (i.e. the sum of all the fitness scores of the individuals in the population).
-*generate_path(): a function to generate a random path in the graph.
-*initialize_population(): a function to initialize a random population of given size.
 """
 
 
@@ -31,27 +21,25 @@ from dataclasses import dataclass
 
 
 
-max = 1000
-START_CITY = 'A'
-END_CITY = 'H'
+max = 999
+START_BLOODBANK = 'A'
+END_BLOODBANK = 'H'
 
 
 # Initialize Parameters.
 
 population_size = 9
-maximum_generations = 99
+max_generations = 399
 mutation_rate = 1
 selection_pressure = 5
-convergence_rate = 0.9
+convergence_rate = 0.86
 threshhold = population_size * max * convergence_rate
-# import pdb;pdb.set_trace()
 
 
 # Initialize Graph.
 
-total_cities = 7
+total_bloodbanks = 7
 
-cities = "ABCDEFH"
 
 position = {
     "A": 0,
@@ -75,24 +63,23 @@ graph = [
 ]
 
 
-# Define genome class.
-# Includes the path taken and fitness.
-# Fitness is calculated the total distance travelled on a given path .
-
 @dataclass
 class Genome:
     path: str
-    fitness: str
+    fitness : int = None
+    
+    def __post_init__(self, ):
+        self.fitness = fitness(self.path)
 
 # Define fitness function.
 
 def fitness(path):
-    """Calculates fitness by adding the distance between all the neigbouring cities in the path,
+    """Calculates fitness by adding the distance between all the neigbouring bloodbanks in the path,
     and return the total distance traversed in a given path."""
 
     total_distance = 0
     for i in range(len(path)-1):
-        # if there is no path between the two cities return max, else add it to the total distance travelled.
+        # if there is no path between the two bloodbanks return max, else add it to the total distance travelled.
         try:
             if graph[position[path[i]]][position[path[i+1]]] == max:
                 return max
@@ -102,26 +89,16 @@ def fitness(path):
     return total_distance
 
 
-def select_population(population, number):
-    """Function to return a random sample of genomes from a given population."""
 
-    selected = []
-    while (len(selected) < number):
-        value = random.randint(0, len(population)-1)
-        if population[value] not in selected:
-            selected.append(population[value])
-    return selected
-
-
-def tournament_selection(population, number):
-    """Return a certain number of fittest individuals in a given population."""
-
+def tournament_selection(population, selection_pressure):
+    """Return selection_pressure number of fittest individuals in a given population."""
+    # import pdb;pdb.set_trace()
     population.sort(key=lambda x: x.fitness)
-    fittest = population[0:number]
+    fittest = population[0:selection_pressure]
     return fittest
 
 
-def ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
+def apply_ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
     """Apply ordered cross (OX) on a genome and return the newly generated genome.
     Pairs of selected individuals (parents) are combined to produce offspring through crossover or
     recombination. This involves exchanging genetic material (e.g., sub-tours in the TSP) between parents to
@@ -131,7 +108,7 @@ def ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
     # Initialize the child with the same length as parents
     p1_path = list(parent_1.path[0:len(parent_1.path)])
     p2_path = list(parent_2.path[0:len(parent_2.path)])
-    child = [START_CITY] + ([-1] * (len(p1_path)-2)) + [END_CITY]
+    child = [START_BLOODBANK] + ([-1] * (len(p1_path)-2)) + [END_BLOODBANK]
 
     # Copy a subset of genes from parent_1 to the child
     child[crossover_start:crossover_end + 1] = p1_path[crossover_start:crossover_end + 1]
@@ -149,11 +126,11 @@ def ordered_crossover(parent_1, parent_2, crossover_start, crossover_end):
                 child[idx_child] = p2_path[idx_parent]
     # print(child)
     # import pdb;pdb.set_trace()
-    return Genome(child, fitness(child))
+    return Genome(child)
 
 
-def mutation(chromosome):
-    """Apply mutation by replacing a random order of cities and return the newly generated genome."""
+def apply_mutation(chromosome):
+    """Apply mutation by replacing a random order of bloodbanks and return the newly generated genome."""
 
     # import pdb;pdb.set_trace()
     offspring = []
@@ -168,84 +145,59 @@ def mutation(chromosome):
         possible_postions.append(i)
 
     for i in range(mutation_rate):
-
         position_1, position_2 = random.sample(possible_postions, 2)
         temp = offspring[position_1]
         offspring[position_1] = offspring[position_2]
         offspring[position_2] = temp
 
-    offspring.extend(END_CITY)
+    offspring.extend(END_BLOODBANK)
     path = "".join(offspring)
-    new_offspring = Genome(path, fitness(path))
+    new_offspring = Genome(path)
     return new_offspring
 
 
-def total_population_score(population):
+def calc_total_population_score(population):
     """Calculate the total score of the population by adding all of their fitness scores."""
 
-    score = 0
-    for individual in population:
-        score += individual.fitness
-    return float(score)
+    return sum([_.fitness for _ in population])
 
-
-def initialize_population(population_size, cities):
+def init_population(population_size, bloodbanks):
     """Function to initialize a random population."""
     
-    def generate_path(cities, start_city, end_city):
+    def generate_path(bloodbanks, start_bloodbank, end_bloodbank):
         """Function to generate a random path."""
-        # start_city = random.choice(cities)
-        remaining_cities = [city for city in cities if city not in [start_city, end_city]]
-        random.shuffle(remaining_cities)
-        # end_city = start_city
-        path = [start_city] + remaining_cities + [end_city]
-
-        '''
-        for i in range(len(remaining_cities)):
-            if i == 6:
-                break
-            city = remaining_cities[i]
-            if city != end_city:
-                path.append(city)
-        path.append(end_city)
-        '''
-        # logger.info(path)
+        remaining_bloodbanks = [bloodbank for bloodbank in bloodbanks if bloodbank not in [start_bloodbank, end_bloodbank]]
+        random.shuffle(remaining_bloodbanks)
+        path = [start_bloodbank] + remaining_bloodbanks + [end_bloodbank]
         return ''.join(path)
     
     
     population = []
 
     for _ in range(population_size):
-        path = generate_path(cities, start_city=START_CITY, end_city=END_CITY)
-        # import pdb;pdb.set_trace()
-        individual = Genome(
-            path, 
-            fitness(path), # Since each genome is a random path in the graph
-        )
+        path = generate_path(bloodbanks, start_bloodbank=START_BLOODBANK, end_bloodbank=END_BLOODBANK)
+        individual = Genome(path)
         population.append(individual)
-    # import pdb;pdb.set_trace()
     return population
 
 
 # Main Function
 
 def main():
-
     # Initialize a population.
-    # import pdb;pdb.set_trace()
-    population = initialize_population(population_size, cities)
-    score = total_population_score(population)
+    population = init_population(population_size, position.keys())
 
-    current_gen = 1
+    current_gen = 0
 
     # fittest = []
 
     # Apply genetic algorithm until a maximum number of generations is reached or the population score is greater than threshhold.
     while True:
-        # import pdb;pdb.set_trace()
+        current_gen += 1
+        score = calc_total_population_score(population)
         print("\n\nGeneration: ", current_gen)
         print("Score: ", score)
-
+        # import pdb;pdb.set_trace()
         fittest_genomes = tournament_selection(population, selection_pressure)
 
         # Print Fittest Genomes after selection.
@@ -262,13 +214,13 @@ def main():
         for i in range(population_size-len(fittest_genomes)):
 
             parent_1, parent_2 = random.sample(possible_parents, 2)
-            new_offspring = ordered_crossover(
+            new_offspring = apply_ordered_crossover(
                 fittest_genomes[parent_1], fittest_genomes[parent_2], 1, 5)
-            mutated_new_offspring = mutation(new_offspring)
+            mutated_new_offspring = apply_mutation(new_offspring)
             new_generation.append(mutated_new_offspring)
 
         for i in range(len(fittest_genomes)):
-            mutated_new_offspring = mutation(fittest_genomes[i])
+            mutated_new_offspring = apply_mutation(fittest_genomes[i])
             new_generation.append(mutated_new_offspring)
 
         # Print the new generation.
@@ -277,19 +229,34 @@ def main():
             print(i.path, "\t", i.fitness)
 
         # Stopping Criterion.
-        if (score <= threshhold or current_gen == maximum_generations):
+        if (score <= threshhold or current_gen >= max_generations):
 
             # Print the generation number and the shorted distance found by the genetic algorithm.
-            print("\n\nGeneration: ", current_gen, "/", maximum_generations)
+            print("\n\nGeneration: ", current_gen, "/", max_generations)
             fittest_genomes.sort(key=lambda x: x.fitness)
             print("Shortest Distance Found:",
                   fittest_genomes[0].path, fittest_genomes[0].fitness)
             break
 
         population = new_generation
-        score = total_population_score(population)
-        current_gen += 1
+        
 
 
 if __name__ == "__main__":
     main()
+"""
+Find and print space and time complexity using code in your implementation.
+Space Complexity Analysis:
+Identify the Inputs:
+
+Similar to the time complexity analysis, identify the inputs that affect the amount of memory used by the algorithm.
+Determine Memory Usage:
+
+Analyze the data structures and variables used in the algorithm to determine how much memory is consumed.
+Determine Dominant Terms:
+
+Identify the most significant terms in memory usage, which typically correspond to the sizes of data structures or the number of recursive calls.
+Express Complexity:
+
+Express the space complexity using Big O notation, focusing on the dominant terms.
+"""
